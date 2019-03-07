e5b68ee6b48c4a29f0eb5749e89218afaf76c066
DESKTOP-F27IQ8Q\JEONG WOO JIN <wjddnwls918@naver.com>
2019-03-08 01:51:56 +0900

    2019-03-08 update

--- Problem/Ac_2609.java
+++ Problem/Ac_2609.java
@@ -1,27 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Ac_2609 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-		int a,b,g;
-		a = in.nextInt();
-		b = in.nextInt();
-		
-		g =getg(a,b);
-		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int a, b, g;
+		String temp[];
+		temp = in.readLine().split(" ");
+		a = Integer.parseInt(temp[0]);
+		b = Integer.parseInt(temp[1]);
+
+		g = getg(a, b);
+
 		System.out.println(g);
-		System.out.println(a*b/g);
-		
+		System.out.println(a * b / g);
+
 	}
-	public static int getg(int a,int b)
-	{
-		if(a %b ==0)
+
+	public static int getg(int a, int b) {
+		if (a % b == 0)
 			return b;
 		else
-		return getg(b, a%b);
-		
+			return getg(b, a % b);
+
 	}
 
 }
